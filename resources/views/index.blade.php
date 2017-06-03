60d1349870502d5eca4711fcd48a97f90758719a
Safak <safak.kocaoglu@bil.omu.edu.tr>
2017-06-03 20:38:56 +0300

    storage has been linked

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Safak.co</title>
+    <link rel="stylesheet" href="{{asset(('storage/web-folder/css/bootstrap.min.css'))}}">
+    <link rel="stylesheet" href="{{asset(('storage/web-folder/css/main.css'))}}">
+    <link rel="stylesheet" href="{{asset(('storage/web-folder/css/font-awesome.min.css'))}}">
+    <link rel="stylesheet" href="{{asset(('storage/web-folder/css/animate.css'))}}">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body>
+its done!
+
+<script src="{{asset(('storage/web-folder/js/jquery-3.2.1.min.js'))}}"></script>
+<script src="{{asset(('storage/web-folder/js/bootstrap.min.js'))}}"></script>
+<script src="{{asset(('storage/web-folder/js/wow.min.js'))}}"></script>
+<script>
+    new WOW().init();
+</script>
+<script src="{{asset(('storage/web-folder/js/main.js'))}}"></script>
+</body>
+</html>
