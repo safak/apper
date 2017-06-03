82a5acfad3b814752486e0d77c4b5d2d1ed0f32f
Safak <safak.kocaoglu@bil.omu.edu.tr>
2017-06-03 20:45:58 +0300

    index has been completed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,245 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 <body>
-its done!
+<section id="home">
+    <nav class="navbar navbar-toggleable-sm navbar-light fixed-top">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#"><img height="35" src="{{asset(('storage/web-folder/images/logo.png'))}}" alt="Golden Website"></a>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#features">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#security">Security</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#faq">FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#download"><b>Download</b></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 home-body wow fadeInLeft">
+                <h3>Simple. Secure..<br>
+                    Reliable messaging...</h3>
+                <p>With our app, you'll get fast, simple, secure messaging and calling for free, available on phones all over the world.</p>
+                <ul class="phone-list">
+                    <li>
+                        <i class="fa fa-android" aria-hidden="true"></i>
+                        <a href="#download">Android</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-apple" aria-hidden="true"></i>
+                        <a href="#download">iPhone</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-windows" aria-hidden="true"></i>
+                        <a href="#download">Windows Phone</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-6 text-center wow fadeInRight">
+                <img src="{{asset(('storage/web-folder/images/phone.png'))}}" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="features">
+    <div class="container">
+        <div class="features-top text-center">
+            <h3>FEATURES</h3>
+            <p>Proin iaculis purus consequat sem cure.</p>
+        </div>
+        <div class="features-content">
+            <div class="row">
+                <div class="col-md-4 text-right wow flipInX">
+                    <h4>Simple, Reliable Messaging</h4>
+                    <p>Proin iaculis purus consequat sem cure
+                        digni ssim. Donec porttitora entum suscipit
+                        aenean rhoncus posuere odio in tincidunt. Proin
+                        iaculis purus consequat sem cure digni
+                        ssim. Donec porttitora entum suscipit.</p>
+                </div>
+                <div class="col-md-4 hidden-sm-down text-center">
+                    <img class="img-circle-features" src="{{asset(('storage/web-folder/images/1.png'))}}" alt="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 offset-sm-4 hidden-sm-down text-center">
+                    <img class="img-circle-features" src="{{asset(('storage/web-folder/images/2.png'))}}" alt="">
+                </div>
+                <div class="col-md-4 wow flipInX">
+                    <h4>Groups to keep in touch</h4>
+                    <p>Proin iaculis purus consequat sem cure
+                        digni ssim. Donec porttitora entum suscipit
+                        aenean rhoncus posuere odio in tincidunt. Proin
+                        iaculis purus consequat sem cure digni
+                        ssim. Donec porttitora entum suscipit.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 text-right wow flipInX">
+                    <h4>Speak Freely</h4>
+                    <p>Proin iaculis purus consequat sem cure
+                        digni ssim. Donec porttitora entum suscipit
+                        aenean rhoncus posuere odio in tincidunt. Proin
+                        iaculis purus consequat sem cure digni
+                        ssim. Donec porttitora entum suscipit.</p>
+                </div>
+                <div class="col-md-4 hidden-sm-down text-center">
+                    <img class="img-circle-features" src="{{asset(('storage/web-folder/images/3.png'))}}" alt="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 offset-sm-4 hidden-sm-down text-center">
+                    <img class="img-circle-features" src="{{asset(('storage/web-folder/images/4.png'))}}" alt="">
+                </div>
+                <div class="col-md-4 wow flipInX">
+                    <h4>Security by Default</h4>
+                    <p>Proin iaculis purus consequat sem cure
+                        digni ssim. Donec porttitora entum suscipit
+                        aenean rhoncus posuere odio in tincidunt. Proin
+                        iaculis purus consequat sem cure digni
+                        ssim. Donec porttitora entum suscipit.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 offset-sm-4 hidden-sm-down text-center">
+                    <img class="img-circle-features" src="{{asset(('storage/web-folder/images/5.png'))}}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="security">
+    <div class="container">
+        <div class="security-top text-center">
+            <h3>SECURITY</h3>
+            <p>Proin iaculis purus consequat sem cure.</p>
+        </div>
+        <div class="security-content">
+            <div class="row">
+                <div class="col-md-6 wow zoomIn">
+                    <img class="img-circle-security" src="{{asset(('storage/web-folder/images/secure.png'))}}" alt="">
+                </div>
+                <div class="col-md-6 wow rotateInUpLeft">
+                    <h4>Privacy and Security is in our DNA</h4>
+                    <p>Our app's end-to-end encryption is available when you and the people you message use the latest versions of our app. Many messaging apps only encrypt messages between you and them, but our app's end-to-end encryption ensures only you and the person you're communicating with can read what is sent, and nobody in between, not even our app. This is because your messages are secured with a lock, and only the recipient and you have the special key needed to unlock and read them. For added protection, every message you send has its own unique lock and key. All of this happens automatically: no need to turn on settings or set up special secret chats to secure your messages.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="faq">
+    <div class="container">
+        <div class="faq-top text-center">
+            <h3>FAQ</h3>
+            <p>Proin iaculis purus consequat sem cure.</p>
+        </div>
+        <div class="faq-content">
+            <div class="faq-c col-md-12">
+                <div class="faq-q"><span class="faq-t">+</span>Why can't I connect to Your App?</div>
+                <div class="faq-a">
+                    <p>If your sent messages only show one check mark, there is nothing wrong with your phone's connection. Please read this article for more information.</p>
+                </div>
+            </div>
+            <div class="faq-c col-md-12">
+                <div class="faq-c">
+                    <div class="faq-q"><span class="faq-t">+</span>How do I verify my number in app?</div>
+                    <div class="faq-a">
+                        <p>This could be an issue with your mobile provider, or it might be an issue with your phone. We need to verify your number and can only do that if you can either receive an SMS or a voice call. We cannot send your code through email.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="faq-c col-md-12">
+                <div class="faq-c">
+                    <div class="faq-q"><span class="faq-t">+</span>How do I restore my messages?</div>
+                    <div class="faq-a">
+                        <p>Message history is removed from the BlackBerry's internal memory after restarting your phone. If your chat history is not saved to a media card (SD card), it will be completely erased after restarting your phone. This is how the BlackBerry system is designed.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="faq-c col-md-12">
+                <div class="faq-c">
+                    <div class="faq-q"><span class="faq-t">+</span>How do I delete messages or chats?</div>
+                    <div class="faq-a">
+                        <p>Open App and go to Options > Settings > Updates.
+                            Click on Check for updates.
+                            Follow the on screen instructions to install the update.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+<section id="download">
+    <div class="container">
+        <div class="download-top text-center">
+            <h3>DOWNLOAD</h3>
+            <p>Proin iaculis purus consequat sem cure.</p>
+        </div>
+        <div class="download-content text-center">
+            <div class="row">
+                <div class="col-md-4 wow fadeInLeft">
+                    <img width="100" src="{{asset(('storage/web-folder/images/android.png'))}}" alt="download for android">
+                    <h4>Android</h4>
+                    <p><a href="#">Download</a> for android device</p>
+                </div>
+                <div class="col-md-4 wow fadeIn">
+                    <img width="100" src="{{asset(('storage/web-folder/images/iphone.png'))}}" alt="download for iphone">
+                    <h4>iPhone</h4>
+                    <p><a href="#">Download</a> for apple device</p>
+                </div>
+                <div class="col-md-4 wow fadeInRight">
+                    <img width="100" src="{{asset(('storage/web-folder/images/windowsphone.png'))}}" alt="download for windows">
+                    <h4>Windows</h4>
+                    <p><a href="#">Download</a> for windows device</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="footer">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-sm-4">
+                <p>&copy; Copyright 2017 <a href="http://safak.co">safak.co</a></p>
+            </div>
+            <div class="col-sm-4">
+                <ul class="company-social-icons text-center">
+                    <li class="company-social-icon">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </li>
+                    <li class="company-social-icon">
+                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                    </li>
+                    <li class="company-social-icon">
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                    </li>
+                    <li class="company-social-icon">
+                        <i class="fa fa-pinterest" aria-hidden="true"></i>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
 
 <script src="{{asset(('storage/web-folder/js/jquery-3.2.1.min.js'))}}"></script>
 <script src="{{asset(('storage/web-folder/js/bootstrap.min.js'))}}"></script>
